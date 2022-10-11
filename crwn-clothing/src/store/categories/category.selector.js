e440b6edceb6880db5443e61cf9798874dd439ec
Marina Knez <marina.knez08@gmail.com>
2022-10-11 19:10:28 +0200

    migrate cart context to redux

--- crwn-clothing/src/store/categories/category.selector.js
+++ crwn-clothing/src/store/categories/category.selector.js
@@ -1,10 +1,18 @@
-export const selectCategoriesMap = (state) => {
-  const categoriesMap = state.categories.categories.reduce(
+import { createSelector } from 'reselect';
+
+const selectCategoriesReducer = (state) => state.categories;
+
+export const selectCategories = createSelector(
+  [selectCategoriesReducer],
+  (categoriesSlice) => categoriesSlice.categories
+);
+
+export const selectCategoriesMap = createSelector(
+  [selectCategories],
+  (categories) => categories.reduce(
     (acc, { title, items }) => {
       acc[title.toLowerCase()] = items;
       return acc;
     },
-    {}
+    {})
   );
-  return categoriesMap;
-};
--- crwn-clothing/src/store/store.js
+++ crwn-clothing/src/store/store.js
@@ -1,9 +1,23 @@
 import { compose, createStore, applyMiddleware } from 'redux';
-import logger from 'redux-logger';
+//import logger from 'redux-logger';
 
 import { rootReducer } from './root-reducer';
 
-const middleWares = [logger];
+const loggerMiddleware = (store) => (next) => (action) => {
+    if(!action.type) {
+        return next(action);
+    }
+
+    console.log('Type: ', action.type);
+    console.log('Payload: ', action.payload);
+    console.log('Current state: ', store.getState());
+
+    next(action);
+
+    console.log('Next state: ', store.getState());
+};
+
+const middleWares = [loggerMiddleware];
 
 const composedEnhancers = compose(applyMiddleware(...middleWares));
 
